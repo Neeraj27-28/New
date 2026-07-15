615bc52dc8c8724b483a590bd5e7cf91cf33b7c2
cpp-Practice <neerru27@gmail.com>
2026-07-15 12:06:32 +0530

    Pin

--- For.cpp
+++ For.cpp
@@ -2,15 +2,23 @@
 using namespace std;
 int main() 
 {
-    int n;
-    int frectional = 1;
-    cout << "Enter a number:"; 
-    cin >> n;
-
-    for(int i = 1; i <= n; i++)
+  int pin; 
+  for (int attempt = 1; attempt <= 3; attempt++) 
+  {
+    cout << "Enter your pin: ";
+    cin >> pin;
+    if (pin == 2728) 
     {
-        frectional = frectional * i;
+      cout << "Access granted!" << endl;
+      break;
+    } 
+    else if (attempt < 3) 
+    {
+      cout << "Access denied. try again." << endl;
+    } 
+    else
+    {
+      cout << "Incorrect pin. Accout Lockes. you have reached your maximum attemapt . Plese contact the bank." << endl;
     }
-    cout << "frectional:" << frectional;
-
+  }
 } 
