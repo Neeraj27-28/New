ae574551f5057dcd203b400ebf7961d34a23ace3
cpp-Practice <neerru27@gmail.com>
2026-07-18 15:17:25 +0530

    do while

--- For.cpp
+++ For.cpp
@@ -1,16 +1,78 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main() 
+int main () 
 {
-    int n;
-    int frectional = 1;
-    cout << "Enter a number:"; 
-    cin >> n;
+  cout << fixed << setprecision(2);
+  int Pin;
+  cout << "Enter your pin:";
+  cin >> Pin;
+  if (Pin == 2728)
+  {
+    cout << "Access granted" << endl;
 
-    for(int i = 1; i <= n; i++)
+    int choice;
+    double balance = 100000.00;
+    do
     {
-        frectional = frectional * i;
-    }
-    cout << "frectional:" << frectional;
+      cout << "======== ATM menue ========" << endl;
+      cout << "1.Check Balance" << endl;
+      cout << "2.Deposite money"<< endl;
+      cout << "3.Withdraw money" << endl;
+      cout << "4.Exit" << endl;
 
-} 
+      cout << "Choose an option:";
+      cin >> choice;
+
+      switch (choice)
+      {
+        case 1:
+          cout << "Your balance is: Rs" << balance << endl;
+          break;
+
+        case 2:
+        {
+          double Deposit;
+          cout << "Enter amount to deposit: Rs";
+          cin >> Deposit;
+
+          balance = balance + Deposit;
+
+          cout<< "Amount successfully deposited!" << endl;
+          cout << "balance: Rs" << balance << endl;
+          break;
+        }
+
+        case 3:
+        {
+          double Withdraw;
+          cout << "Enter amount to withdraw: Rs";
+          cin >> Withdraw;
+          if (Withdraw <= balance)
+          {
+            balance = balance - Withdraw;
+            cout << "Amount successfully Withdraw!" << endl;
+            cout << "Remaning balance: Rs" << balance << endl;
+          }
+          else
+          {
+            cout << "balance was not sufficient!" << endl;
+          }
+          break;
+        }
+
+        case 4:
+          cout << "thanks for using our ATM mechaine!" << endl;
+          break;
+
+        default:
+          cout << "choose the correct option!" << endl;
+      }
+
+    } while (choice != 4);
+  }
+  else
+  {
+    cout << "Access denied" << endl;
+  }
+}
